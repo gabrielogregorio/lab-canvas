7e2c6c939ea569fe0200fec972bc4294226170f4
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2023-10-11 17:17:03 -0300

    feat: added animation circles

--- circle.ts
+++ circle.ts
@@ -1,16 +1,34 @@
 (function () {
-  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-
   function angleToRadians(angle: number) {
     return (Math.PI / 180) * angle;
   }
 
-  function draw() {
-    let radius = 50;
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+  const widthCanvas = 500;
+
+  const renderRandomCircle = (widthCanvas: number) => {
+    context.beginPath();
+    context.fillStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    const positionX = Math.random() * widthCanvas;
+    const positionY = Math.random() * widthCanvas;
+    const radius = Math.random() * 100;
+    const startAngle = angleToRadians(0);
+    const endAngle = angleToRadians(360);
+    const counterclockwise = true;
+
+    context.arc(positionX, positionY, radius, startAngle, endAngle, counterclockwise);
+    context.fill();
+    context.closePath();
+  };
+
+  const renderAnimateCircle = (endAngleControl: number) => {
     const startAngle = angleToRadians(180);
-    const endAngle = angleToRadians(45);
     const counterclockwise = false;
+
+    let radius = 50;
+    const endAngle = angleToRadians(endAngleControl);
 
     context.beginPath();
     context.fillStyle = "#0aa";
@@ -23,6 +41,27 @@
     context.arc(200, 200, radius, startAngle, endAngle, !counterclockwise);
     context.fill();
     context.closePath();
+  };
+
+  const quantitySpheres = 50;
+
+  let endAngleControl = 0;
+  function draw() {
+    setTimeout(() => {
+      context.clearRect(0, 0, 500, 500);
+
+      for (let index = 0; index < quantitySpheres; index += 1) {
+        renderRandomCircle(widthCanvas);
+      }
+
+      renderAnimateCircle(endAngleControl);
+      endAngleControl += 1;
+      if (endAngleControl > 360) {
+        endAngleControl = 0;
+      }
+
+      requestAnimationFrame(draw);
+    }, 800);
   }
 
   draw();
