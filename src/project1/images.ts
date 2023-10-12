3ded9c07ffcd74a0fc09023caa12533bd041d218
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2023-10-12 11:10:47 -0300

    feat: added keyboard handler

--- src/project1/images.ts
+++ src/project1/images.ts
@@ -1,43 +1,14 @@
+import { KeyBoardHandler } from "./KeyBoardHandler";
 import { Person } from "./Person";
-import { keyboardStateType } from "./types";
 
 (async function () {
   const canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
   const canvasContext = canvasElement.getContext("2d") as CanvasRenderingContext2D;
 
-  let keyboardState: keyboardStateType = {
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  };
-
-  const person = new Person(canvasContext, keyboardState);
+  const keyboard = new KeyBoardHandler();
+  const person = new Person(canvasContext, keyboard.keyboardState);
 
   await person.loadFiles();
-
-  const moves = {
-    w(newValue) {
-      keyboardState.top = newValue;
-    },
-    s(newValue) {
-      keyboardState.bottom = newValue;
-    },
-    a(newValue) {
-      keyboardState.left = newValue;
-    },
-    d(newValue) {
-      keyboardState.right = newValue;
-    },
-  };
-
-  window.addEventListener("keydown", (event) => {
-    moves[event.key]?.(true);
-  });
-
-  window.addEventListener("keyup", (event) => {
-    moves[event.key]?.(false);
-  });
 
   const draw = () => {
     canvasContext.clearRect(0, 0, 500, 500);
