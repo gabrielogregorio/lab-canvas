49484490ca49bb1389565223c137e47fd1b06cf6
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2023-10-11 21:32:40 -0300

    feat: added api static files

---
+++ api.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static("./static"));
+
+app.listen(8080);
